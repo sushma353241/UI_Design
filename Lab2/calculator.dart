0109888f990805e59ea97862a0d4e4a08495ce50
sushma353241 <152761116+sushma353241@users.noreply.github.com>
2025-03-06 10:10:21 +0530

    Update calculator.dart

--- Lab2/calculator.dart
+++ Lab2/calculator.dart
@@ -1,1 +1,17 @@
-
+import 'dart:io';
+void main()
+{
+	print("Enter the value of a");
+	int a =int.parse(stdin.readLineSync()!);
+	print("Enter the value of b");
+	int b = int.parse(stdin.readLineSync()!);
+	print(sum(a,b));
+	print(sub(a,b));
+	print(mul(a,b));
+	print(div(a,b));
+	
+}
+int sum(a,b) => a+b;
+int sub(a,b) => b-a;
+int div(a,b) => a~/b;
+int mul(a,b) => a*b;
