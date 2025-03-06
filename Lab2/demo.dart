13df52b36e87ae0de0ba4d2f5eb5ff122044cfd2
sushma353241 <152761116+sushma353241@users.noreply.github.com>
2025-03-06 10:09:45 +0530

    Update demo.dart

--- Lab2/demo.dart
+++ Lab2/demo.dart
@@ -1,1 +1,34 @@
-
+import 'dart:io';
+void main()
+{
+	print("Enter the value of a");
+	int a =int.parse(stdin.readLineSync()!);
+	print("Enter the value of b");
+	int b = int.parse(stdin.readLineSync()!);
+	sum(a,b);
+	sub(a,b);
+	div(a,b);
+	mul(a,b);
+}
+void sum(int num1, int num2)
+{
+	print('The addition of numbers is:');
+	print(num1+num2);
+}
+void sub(int num1, int num2)
+{
+	print('The subtraction of numbers is:');
+	print(num2-num1);
+}
+void div(int num1,int num2)
+{
+	
+	print('The division of numbers is:');
+	print(num2/num1);
+}
+void mul(int num1,int num2)
+{
+	print('The multiplication of numbers is:');
+	print(num1*num2);
+}
+	
