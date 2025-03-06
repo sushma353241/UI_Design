d0d1260fc939a57fed72276bb7544665fb915349
sushma353241 <152761116+sushma353241@users.noreply.github.com>
2025-03-06 10:10:58 +0530

    Update biggest.dart

--- Lab2/biggest.dart
+++ Lab2/biggest.dart
@@ -1,1 +1,22 @@
+import 'dart:io';
+void main()
+{
+	print("Enter the value of a");
+	int a =int.parse(stdin.readLineSync()!);
+	print("Enter the value of b");
+	int b = int.parse(stdin.readLineSync()!);	
+	biggest(a,b);
+}
+void biggest(int a,int b)
+{
+	if(a>b)
+	{
+		stdout.write('The big number is: ${a}');
+	}
+	else
+	{
+		stdout.write('The big number is: ${b}');
+	}
+}
+
 
